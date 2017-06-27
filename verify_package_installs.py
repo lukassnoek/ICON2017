2cb1ee64d42d4a9f1f08017219f22e339b8c5963
Lukas Snoek <lukassnoek@gmail.com>
2017-06-27 16:34:50 +0200

    Add verification script

---
+++ verify_package_installs.py
@@ -0,0 +1,13 @@
+from importlib import import_module
+
+packages = ['sklearn', 'nibabel', 'numpy',
+            'matplotlib']
+
+for package in packages:
+    try:
+        import_module(package)
+        print('%s was succesfully installed!' % package)
+    except ImportError:
+        print('\nCould not find package %s' % package)
+        cmd = "pip install %s" % package
+        print('Install %s using the command: %s\n' % (package, cmd))
