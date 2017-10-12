848af4467571752b06233ba33290aa1276ab0f2f
Lukas Snoek <lukassnoek@gmail.com>
2017-10-12 10:26:40 +0200

    Add scipy

--- verify_package_installs.py
+++ verify_package_installs.py
@@ -3,7 +3,7 @@ import os
 from importlib import import_module
 
 packages = ['sklearn', 'nibabel', 'numpy',
-            'matplotlib', 'skbold']
+            'matplotlib', 'skbold', 'scipy']
 
 warnings.filterwarnings("ignore")
 
